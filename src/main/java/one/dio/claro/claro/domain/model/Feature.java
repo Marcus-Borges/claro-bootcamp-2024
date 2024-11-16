2a105bebf9a7925007020d86887a556339bec6e1
Marcus Borges <marcus.borges@gmail.com>
2024-11-15 21:32:11 -0300

    Implementação da camada de dominio

---
+++ src/main/java/one/dio/claro/claro/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package one.dio.claro.claro.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/one/dio/claro/claro/domain/model/News.java
@@ -0,0 +1,8 @@
+package one.dio.claro.claro.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
