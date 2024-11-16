95cf9129290f00f73012d10e94c0fb3509d8c6ff
Marcus Borges <marcus.borges@gmail.com>
2024-11-16 15:08:32 -0300

    Primeira tentativa de publicação no Railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/claro-bootcamp-2024-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/one/dio/claro/claro/service/UserService.java
@@ -0,0 +1,8 @@
+package one.dio.claro.claro.service;
+
+import one.dio.claro.claro.domain.model.User;
+import one.dio.claro.claro.service.impl.CrudService;
+
+public interface UserService extends CrudService<Long, User> {
+
+}
